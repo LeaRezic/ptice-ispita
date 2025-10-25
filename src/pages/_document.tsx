3c443c84ed1281a003505f3486c2a8a3afbb1b78
Lea Rezic <lea.rezic@productive.io>
2025-10-25 11:11:29 +0200

    Revive favicon, hopefully

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="shortcut icon"
+          href={process.env.NODE_ENV === 'production' ? "/ptice-ispita/favicon.ico" : "/favicon.ico"}
+          type="image/x-icon"
+          sizes="16x16"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
